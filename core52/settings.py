9f3541a87d52259cc35233aa6a7418246c5b0cbb
Dostin Umaña <24000286@galileo.edu>
2025-06-04 11:23:45 -0600

    Refactor code structure for improved readability and maintainability

--- core52/settings.py
+++ core52/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
 TIME_ZONE = 'UTC'
 
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- eventos/views.py
+++ eventos/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-
+from .models import evento
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+
+    # listado = evento.objects.all().order_by('fecha_Inicio')
+    listado_eventos = evento.objects.all().filter(activo=True).order_by('fecha_Inicio')
+    # listado_eventos = evento.objects.all()
+
+    data = {
+        'title': 'Eventos',
+        'listado': listado_eventos,
+    }
+
+    # data = {
+    #     'title': 'Eventos',
+    #     'parrafo': 'bienvenido al sistema de eventos!',
+    #     'eventos': [
+    #         {'nombre': 'Concierto de Rock', 'fecha': '2023-10-01', 'lugar': 'Estadio Nacional'},
+    #         {'nombre': 'Feria de Tecnología', 'fecha': '2023-10-15', 'lugar': 'Centro de Convenciones'},
+    #         {'nombre': 'Festival de Cine', 'fecha': '2023-11-05', 'lugar': 'Cinepolis'},
+    #     ]
+    # }
+    return render(request, 'home.html', data)
 
 def about(request):
     return render(request, 'about.html')
